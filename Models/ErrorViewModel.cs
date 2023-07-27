500af4cd95bba41fad2c7cc334d185b0891161c4
Ejekwu Graveth <ejekwugraveth2016@gmail.com>
2023-07-27 01:11:11 +0100

    custom identity authorization

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace G_CustomeIdentity.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
